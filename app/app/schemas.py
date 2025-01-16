c8730a1eec9a86895fdc3066962cf59895271e2c
deadonted <timofejmitusin60892@gmail.com>
2025-01-17 02:19:27 +0300

    feat: add pydantic dchemas

--- app/app/schemas.py
+++ app/app/schemas.py
@@ -1,76 +1,22 @@
-from datetime import datetime
-from typing import List
-from pydantic import BaseModel
+from typing import Optional
+
+from pydantic import BaseModel, Field
 
 
 class ApplicationCreate(BaseModel):
-	user_name: str
-	description: str
+    user_name: str = Field(...)
+    description: str = Field(...)
 
-	class Config(object):
-		json_schema_extra = {
-			'example': {
-				'user_name': 'John',
-				'description': 'Hello, I am John. This is my example description.',
-			}
-		}
-
-
-class ApplicationResponse(BaseModel):
-	id: str
-	user_name: str
-	description: str
-	created_at: datetime
-
-	class Config(object):
-		json_schema_extra = {
-			'example': {
-				'id': '123e4567-e89b-12d3-a456-426655440000',
-				'user_name': 'John',
-				'description': 'Hello, I am John. This is my example description.',
-				'created_at': '2025-01-14T22:00:00'
-			}
-		}
+    class Config:
+        json_schema_extra = {
+            'example': {
+                'user_name': 'John',
+                'description': 'Hello, I am John. This is my example description.',
+            }
+        }
 
 
 class ApplicationsListGet(BaseModel):
-	page: int 
-	size: int
-
-	class Config(object):
-		json_schema_extra = {
-			'example': {
-				'page': 1,
-				'size': 10,
-			}
-		}
-
-
-class ApplicationsListResponse(BaseModel):
-	page: int
-	size: int
-	total: int
-	applications: List[ApplicationResponse]
-
-	class Config(object):
-		json_schema_extra = {
-			'example': {
-				'page': 1,
-				'size': 10,
-				'total': 2,
-				'applications': [
-					{
-                        'id': '123e4567-e89b-12d3-a456-426655440000',
-                        'user_name': 'John',
-                        'description': 'Hello, I am John. This is my example description.',
-                        'created_at': '2025-01-14T22:00:00',
-                    },
-                    {
-                        'id': '593j4757-y89g-12d3-m456-499855440157',
-                        'user_name': 'Jane',
-                        'description': 'Hi, I am Jane. This is my example description.',
-                        'created_at': '2025-01-14T23:00:00',
-                    },
-				]
-			}
-		}
+    page: int = Field(default=1, ge=1)
+    size: int = Field(default=10, ge=1, le=100)
+    user_name: Optional[str] = None
